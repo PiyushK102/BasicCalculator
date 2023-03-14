72b902259bde48922b214cf003fa0cf5224f281b
Piyush Kachhwal <piyushkachhwal1812@gmail.com>
2023-03-14 23:11:26 +0530

    Add files via upload

--- calci.js
+++ calci.js
@@ -1,20 +1,26 @@
+
+
+
 function append(value){
-        
     document.getElementById('result').innerHTML+=value;
 }
 function clearInput(){
     document.getElementById('result').innerHTML="";
 }
 function calculate(){
-    var ans=eval(cal);
-    document.getElementById('result').innerHTML=ans;
+    if(document.getElementById('result').innerHTML.includes("^"))
+    {
+       var cal= document.getElementById('result').innerHTML.replace("^","**");
+        document.getElementById('result').innerHTML=eval(cal)
+    }
+    else{
+        var ans=eval(document.getElementById('result').innerHTML);
+        document.getElementById('result').innerHTML=ans;
+    }
 }
 function backspace(){
     document.getElementById('result').innerHTML=document.getElementById('result').innerHTML.slice(0,(result.innerHTML.length-1))
 }
 function sqrt(){
     document.getElementById('result').innerHTML=Math.sqrt(document.getElementById('result').innerHTML);
-}
-function power(){
-    document.getElementById('result').innerHTML=Math.cbrt(document.getElementById('result').innerHTML);
 }
