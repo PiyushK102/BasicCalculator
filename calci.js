be1605e43ce80162c24fe9e383d60765050e5b7e
Piyush Kachhwal <piyushkachhwal1812@gmail.com>
2023-06-25 23:15:57 +0530

    Add files via upload

--- calci.js
+++ calci.js
@@ -1,6 +1,4 @@
 
-
-
 function append(value){
     document.getElementById('result').innerHTML+=value;
 }
@@ -24,3 +22,71 @@ function backspace(){
 function sqrt(){
     document.getElementById('result').innerHTML=Math.sqrt(document.getElementById('result').innerHTML);
 }
+
+///////////////////////////////////////////////////------------Keyboard Control------------/////////////////////////////////////////////////////////////////
+document.addEventListener("keydown", e =>{
+    switch(e.key.toLowerCase()){
+        case "1":
+            append(1)
+            break
+        case "2":
+            append(2)
+            break 
+        case "3":
+            append(3)
+            break
+        
+        case "4":
+            append(4)
+            break
+        case "5":
+            append(5)
+            break
+        case "6":
+            append(6)
+            break
+        case "7":
+            append(7)
+            break
+        case "8":
+            append(8)
+            break
+        case "9":
+            append(9)
+            break
+        case "0":
+            append(0)
+            break
+        case "=":
+            case "enter":
+            calculate()
+            break
+        case "+":
+            append("+")
+            break
+        case "-":
+            append("-")
+            break
+        case "*":
+            append("*")
+            break
+        case "/":
+            append("/")
+            break
+        case "^":
+            append("^")
+            break
+        case "backspace":
+            backspace()
+            break
+        case "c":
+            clearInput()
+            break
+        case "s":
+            sqrt()
+            break
+        case ".":
+            append(".")
+            break
+    }   
+})
